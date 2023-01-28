38eec60cabfaa920c3bb2dedb5b415e896d2dddf
CrowTamer <91907309+CrowTamerCodes@users.noreply.github.com>
2023-01-28 16:04:31 -0500

    Update fenrir.js

--- fenrir.js
+++ fenrir.js
@@ -9,54 +9,22 @@
 		Bitburner V2.2.1 javascript
 Single server hack with weaken and grow mechanics
 */
-/*
-async function serverWeaken(ns, currentSecurity, minSecurity, currentServer){
-	
-	//var targetSecurityLvl = (ns.getServer(currentServer).baseDifficulty / 2);
-	var targetSecurityLvl = (minSecurity * 3);
-	if (currentSecurity >= targetSecurityLvl)
-	{	
-		
-		ns.print("security too high");
-		var iterations = 0;
-		while (currentSecurity >= targetSecurityLvl)
-		{
-			await ns.weaken(currentServer);
-			currentSecurity = ns.getServerSecurityLevel(currentServer);
-			iterations++;
-			ns.print("running weaken step " + iterations);
-		}
-		ns.print("Server Broken")
-	}
-}
 
-async function growth(ns, minMoney, currentMoney, maxMoney, currentServer){
-	
-	if (currentMoney <= minMoney)
-	{
-		ns.print("funds below threshold");
-		while (currentMoney <= maxMoney)
-		{
-			await ns.grow(currentServer);
-			currentMoney = ns.getServerMoneyAvailable(currentServer);
-		}
-		ns.print("Server Full")
-	}
-	
-}
-*/
 async function jobSelect(ns, currentServer){
 
-	var timeToHack = ns.getHackTime(currentServer);
-	var timeToWeaken = ns.getWeakenTime(currentServer);
-	var timeToGrow = ns.getGrowTime(currentServer);
+	//Money Vars
+	var maxMoney = ns.getServerMaxMoney(currentServer);
+	var currentMoney = ns.getServerMoneyAvailable(currentServer);
+	//Security Vars
+	var currentSec = ns.getServerSecurityLevel(currentserver);
+	var minSec = ns.getServerMinSecurityLevel(currentServer);
 	//might need to change order and make weaken the default method.
 
-	if (timeToGrow <= timeToWeaken && timeToGrow <= timeToHack)
+	if (currentMoney <= maxMoney * 0.75)
 	{
 		await ns.grow(currentServer);
 
-	} else if (timeToWeaken <= timeToHack && timeToWeaken <= timeToGrow) {
+	} else if (currentSec >= minSec *2) {
 
 		await ns.weaken(currentServer);
 
@@ -69,21 +37,8 @@ async function jobSelect(ns, currentServer){
 }
 
 export async function main(ns) {
-	/*
-	var currentServer = ns.getHostname();
-	var maxMoney = ns.getServerMaxMoney(currentServer);
-	var currentMoney = ns.getServerMoneyAvailable(currentServer);
-	var moneyThreshold = 50000;
-	var minSecurity = ns.getServerMinSecurityLevel(currentServer);
-	var currentSecurity = ns.getServerSecurityLevel(currentServer);
-	*/
-	//ns.tail(ns.getScriptName());
 
 	while(true) {
-		//currentMoney = ns.getServerMoneyAvailable(currentServer);
-		//currentSecurity = ns.getServerSecurityLevel(currentServer);
-		//await serverWeaken(ns, currentSecurity, minSecurity, currentServer);
-		//await growth(ns, moneyThreshold, currentMoney, maxMoney, currentServer);
 		//await ns.hack(currentServer);
 		var currentServer = ns.getHostname();
 		await jobSelect(ns, currentServer);
